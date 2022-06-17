23fd2aa2cd4d072d01a7d45b891e6c00c97fb2f7
Bbromw <kaystim6@gmail.com>
2022-06-17 14:08:40 +0700

    add animaton

--- javascript/main.js
+++ javascript/main.js
@@ -7,4 +7,22 @@ button.addEventListener("click", function(){
   else{
     nav.add("active")
   }
+});
+
+const element = document.querySelectorAll(".scroll")
+const windowHeight = window.innerHeight
+window.addEventListener("scroll", () => {
+  for(let i = 0; i < element.length; i++){
+    const elementTop = element[i].getBoundingClientRect().top
+    console.log(elementTop)
+    if(elementTop < windowHeight - 650){
+      element[i].classList.add("active-animation")   
+    }
+    else
+      element[i].classList.remove("active-animation")  
+  }
 })
+
+
+
+
